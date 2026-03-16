eda9e6bec042b3bb8e0b1092ee9c477649f5b867
ThePotatoGamer0 <102364508+ThePotatoGamer0@users.noreply.github.com>
2026-03-16 02:19:10 +0000

    fix vercel routing again again...

--- apps/time/vite.config.js
+++ apps/time/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/time/',
+  build: {
+    outDir: '../../dist/time' // Builds to root/dist/time
+  }
 })
